fdcfc86c5125edd72efc516259b7926661b084eb
Joshua Pott <pott.joshy@gmail.com>
2024-04-10 10:36:40 +0200

    Aurufen der Verbindung im Konstruktor

--- ServerMonitoring.WorkerService/Worker.cs
+++ ServerMonitoring.WorkerService/Worker.cs
@@ -7,21 +7,22 @@ public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
     private readonly IPConnection _ipConnection;
+    private readonly BrickletPTCV2 _brickletPtcv2;
     private static string HOST = "172.20.10.242";
     private static int PORT = 4223;
     private static string UID = "R7M"; // Change XXYYZZ to the UID of your Stepper Brick
+    
     public Worker(ILogger<Worker> logger, IPConnection ipConnection)
     {
         _logger = logger;
-        _ipConnection = ipConnection;   
+        _ipConnection = ipConnection;
+        _ipConnection.Connect(HOST, PORT); // Verbindung hier aufbauen
+        _brickletPtcv2 = new BrickletPTCV2("Wcg", _ipConnection); // Device hier initialisieren
     }
 
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        
-        var device = new BrickletPTCV2("Wcg", _ipConnection);
-        _ipConnection.Connect(HOST, PORT);
         var temperatureRespone = HandleTemperatureAsync(device);
         if (temperatureRespone.Status == TemperatureStatus.High)
         {
