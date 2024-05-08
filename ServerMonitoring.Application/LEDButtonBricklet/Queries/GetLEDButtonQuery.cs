6322d4d86e8fbeeb489903078d761ae1cd1d5c66
FrydrycM <frydrycm@apetito.com>
2024-05-08 10:37:49 +0200

    pull request changes

--- ServerMonitoring.Application/LEDButtonBricklet/Queries/GetLEDButtonQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using ServerMonitoring.Application.Responses;
-
-namespace ServerMonitoring.Application.LEDButtonBricklet.Queries;
-
-public class GetLEDButtonQuery : IRequest<LEDButtonResponse>;
