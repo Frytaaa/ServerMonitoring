72f1d8d4283a2714cc6eb5022f4be894ade15362
FrydrycM <frydrycm@apetito.com>
2024-06-03 00:56:23 +0200

    refactoring

--- ServerMonitoring.Application/E-Paper/Querie/GetEPaperQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using ServerMonitoring.Application.Responses;
-
-namespace ServerMonitoring.Application.E-Paper.Queries;
-
-public class GetEPaperQuery
-{
-    public class GetEPaperQuery : IRequest<EPaperResponse>;
-}
