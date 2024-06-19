2436381798d3bc155293b60dfc0d26f2dc4be4de
FrydrycM <frydrycm@apetito.com>
2024-06-19 10:55:42 +0200

    restore removed code

--- ServerMonitoring.Application/BrickletHumidityV2/HumidityCallback.cs
+++ ServerMonitoring.Application/BrickletHumidityV2/HumidityCallback.cs
@@ -4,7 +4,7 @@ namespace ServerMonitoring.Application.BrickletHumidityV2;
 
 public static class HumidityCallback
 {
-    public static async void GetHumidityCallback(Tinkerforge.BrickletHumidityV2 sender, int humidity)
+    public static void GetHumidityCallback(Tinkerforge.BrickletHumidityV2 sender, int humidity)
     {
         Console.WriteLine("Humidity: " + humidity/100.0 + " %RH");
         Console.WriteLine("Recommended humidity for human comfort is 40 to 60 %RH.");
